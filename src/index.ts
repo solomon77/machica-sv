7a4595121624db33944cd31a357335f625d01e7b
solomon77 <who.stole.the.weekend@gmail.com>
2025-09-21 01:07:40 +0900

    debug cors

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,12 @@ app.use(
       "https://asobiba-machica-test.web.app", // Staging Frontend
       "http://localhost:5173", // Local Development
     ],
-    allowHeaders: [
-      "X-Custom-Header",
-      "Upgrade-Insecure-Requests",
-      "Content-Type",
-    ],
-    allowMethods: ["POST", "GET", "OPTIONS"],
+    // allowHeaders: [
+    //   "X-Custom-Header",
+    //   "Upgrade-Insecure-Requests",
+    //   "Content-Type",
+    // ],
+    // allowMethods: ["POST", "GET", "OPTIONS"],
   })
 );
 
