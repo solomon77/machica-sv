bf61771fd2fe3e09d7a576e1db31f20f0db1ff32
solomon77 <who.stole.the.weekend@gmail.com>
2025-09-21 20:20:38 +0900

    added products api handler

--- src/features/products/products.api.handler.ts
+++ src/features/products/products.api.handler.ts
@@ -1,21 +1,30 @@
 import { Context } from "hono";
+import { stripe } from "../../lib/stripe";
 
 export default async function productsApiHandler(c: Context) {
-  const {} = c;
+  try {
+    /**
+     * 1. Stripeから有効な商品と価格を取得
+     */
+    const [stripeProducts, stripePrices] = await Promise.all([
+      stripe.products.list({ active: true }),
+      stripe.prices.list({ active: true }),
+    ]);
 
-  /**
-   * 1. stripeの stripe.products.list を取得
-   */
+    /**
+     * 2. 商品と価格をマージ
+     */
+    const productsWithPrices = stripeProducts.data.map((pd) => {
+      const prices = stripePrices.data.filter((pr) => pr.product === pd.id);
+      return { ...pd, prices };
+    });
 
-  /**
-   * 2. stripeの stripe.prices.list を取得
-   */
-
-  /**
-   * 3. productsとpriceをマージ
-   */
-
-  /**
-   * 4. フロントに3を返す
-   */
+    /**
+     * 3. マージしたデータをクライアントに返す
+     */
+    return c.json(productsWithPrices);
+  } catch (error) {
+    console.error("Stripeからの商品取得中にエラーが発生しました:", error);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
 }
